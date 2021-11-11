b73840966a432faf0f6200c1cf76308639b7c7b3
Piao <prihe89@gmail.com>
2021-11-11 19:25:15 +0300

    updated app for heroku deploy

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import serveStatic from 'serve-static';
+import path from 'path';
+
+const app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 3000;
+app.listen(port);
